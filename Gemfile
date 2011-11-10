1f137484b88c5b2ae0c4d8d88ecc49a625993c94
Vedran <vedranb@gmail.com>
2011-11-10 05:30:14 -0800

    switched to oilypng/chunkypng for big speed boost

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source :rubygems
 
 gem "rmagick", "~>2.13.1"
+gem "chunky_png"
+gem "oily_png"
--- whiteboard.rb
+++ whiteboard.rb
@@ -1,17 +1,18 @@
-require 'RMagick'
-include Magick
+#require 'RMagick'
+require 'oily_png'
+#include Magick
+
+@min_redness = 110
+@max_rect_axis_difference = 30
 
 class Blob
 
-	attr_reader :points, :min_x, :min_y, :max_x, :max_y, :avg_x, :avg_y
+	attr_reader :points, :min_x, :min_y, :max_x, :max_y
 
 	@@number_of_blobs = 0
 
 	def initialize
 		@points = Array.new
-
-		@avg_x = 0
-		@avg_y = 0
 
 		@min_x = nil
 		@min_y = nil
@@ -28,8 +29,6 @@ class Blob
 		@max_y = y if @max_y.nil? || y > @max_y
 
 		@points << [x, y]
-		@avg_x = ((@avg_x * (@points.length-1)) + x) / (@points.length)
-		@avg_y = ((@avg_y * (@points.length-1)) + y) / (@points.length)
 	end
 
 	def center_point
@@ -37,8 +36,8 @@ class Blob
 	end
 end
 
-def quantumify(number)
-	(number.to_f / 255.0) * Magick::QuantumRange
+def calc_redness(r, g, b)
+	(2 * r) - (g + b)
 end
 
 def is_blob_rect?(blob)
@@ -52,7 +51,6 @@ def is_blob_rect?(blob)
 
 	#a square placed on top of the blob
 	#check the percentage of pixels that don't 'fill' the square, or go past the square
-#	puts blob.avg_x.to_s + ", " + blob.avg_y.to_s
 #	puts "-----------------------------------"
 #	puts "top left (" + left.to_s + ", " + top.to_s + ")  -  bottom right: (" + right.to_s + ", " + bottom.to_s + ")"
 #	puts "width : " + width.to_s + ", height: " + height.to_s + " width / height = " + (width.to_f / height.to_f).to_s
@@ -60,13 +58,15 @@ def is_blob_rect?(blob)
 	coverage = (blob.points.uniq.length.to_f / (width * width).to_f).to_f
 	sides_ratio = (width.to_f / height.to_f) 
 #	puts "coverage: " + coverage.to_s
+#	puts "sides_ratio: " + sides_ratio.to_s
+	
 #	puts blob.min_x.to_s + " - " + blob.max_x.to_s + ", " + blob.min_y.to_s + " - " + blob.max_y.to_s + ", total px: " + blob.points.uniq.length.to_f.to_s + ", " + "w: " + width.to_s + ", h: " + height.to_s + " : cov: " + coverage.to_s
 	
-	coverage >= 0.65 && coverage <= 1.35 && sides_ratio >= 0.75 && sides_ratio <= 1.25
+	coverage >= 0.65 && coverage <= 1.35 && sides_ratio >= 0.75 && sides_ratio <= 1.35
 end
 
 def in_image_bounds?(image, x, y)
-	x > 0 && x < image.columns && y > 0 && y < image.rows
+	x > 0 && x < image.dimension.width && y > 0 && y < image.dimension.height
 end
 
 def neighbours(image, x, y)
@@ -76,7 +76,7 @@ def neighbours(image, x, y)
 	end
 end
 
-def find_blob(view, image, blobs, x, y)
+def find_blob(image, blobs, x, y)
 	blob = Blob.new
 	stack = []
 	stack << [x, y]
@@ -85,25 +85,25 @@ def find_blob(view, image, blobs, x, y)
 		next_pixel = stack.pop
 		blob.add_point(*next_pixel)
 
-		view[next_pixel[1]][next_pixel[0]] = Pixel.new(quantumify(254), quantumify(254), quantumify(254))
-#		view[next_pixel[1]][next_pixel[0]].blue = quantumify(255)
+		image[next_pixel[0], next_pixel[1]] = ChunkyPNG::Color.rgb(0, 0, 255)
 
 		neighbours(image, *next_pixel).each do |xy|
-			if view[xy[1]][xy[0]].red == quantumify(255)
+			redness = calc_redness(ChunkyPNG::Color.r(image[xy[0], xy[1]]), ChunkyPNG::Color.g(image[xy[0], xy[1]]),
+						ChunkyPNG::Color.b(image[xy[0], xy[1]]))
+			if redness > @min_redness
 				stack << [xy[0], xy[1]]
 			end
 		end
 	end
 
-	blobs << blob if blob.points.uniq.length > 150 && is_blob_rect?(blob)
+	blobs << blob if blob.points.uniq.length > 200 && is_blob_rect?(blob)
 end
 
-def find_bounding_rect(view, image, blobs)
+def find_bounding_rect(image, blobs)
 
 	#the first blob I run into scanning left -> right, top -> bottom
 	#should be either the top left blob or the top right blob
 
-	max_difference_value = 20
 
 	blobs.map do |top_blob|
 
@@ -121,14 +121,14 @@ def find_bounding_rect(view, image, blobs)
 
 			#matched a blob below
 #			puts "below: comparing: " + top_blob.center_point[0].to_s + " to " + next_blob.center_point.to_s
-			if (top_blob.center_point[0] - next_blob.center_point[0]).abs < max_difference_value
+			if (top_blob.center_point[0] - next_blob.center_point[0]).abs < @max_rect_axis_difference
 #				puts "matched bottom: " + top_blob.center_point.to_s + " and " + next_blob.center_point.to_s
 				bottom_blob = next_blob
 				next
 			end
 
 #			puts "side: comparing: " + top_blob.center_point[0].to_s + " to " + next_blob.center_point.to_s
-			if (top_blob.center_point[1] - next_blob.center_point[1]).abs < max_difference_value
+			if (top_blob.center_point[1] - next_blob.center_point[1]).abs < @max_rect_axis_difference
 #				puts "top_side matched: " + top_blob.center_point.to_s + " and " + next_blob.center_point.to_s
 				top_side_blob = next_blob
 				next
@@ -139,8 +139,8 @@ def find_bounding_rect(view, image, blobs)
 				
 #				puts "top side: " + top_side_blob.center_point.to_s + ", bottom_side: " +  next_blob.center_point.to_s
 #				puts "bottom: " + top_side_blob.center_point.to_s + ", bottom_side: " +  next_blob.center_point.to_s
-				if (top_side_blob.center_point[0] - next_blob.center_point[0]).abs < max_difference_value &&
-					(bottom_blob.center_point[1] - next_blob.center_point[1]).abs < max_difference_value
+				if (top_side_blob.center_point[0] - next_blob.center_point[0]).abs < @max_rect_axis_difference &&
+					(bottom_blob.center_point[1] - next_blob.center_point[1]).abs < @max_rect_axis_difference
 					
 					bottom_side_blob = next_blob
 #					puts "identified rectangle! at: " + top_blob.center_point.to_s + " , " + bottom_side_blob.center_point.to_s
@@ -149,10 +149,12 @@ def find_bounding_rect(view, image, blobs)
 
 					#if we have the top left corner as the top blob
 					if top_blob.center_point[0] < bottom_side_blob.center_point[0]
-						return  [[top_blob.max_x, top_blob.max_y], [bottom_side_blob.min_x, bottom_side_blob.min_y]]
+						return  [[top_blob.max_x, top_blob.max_y], [bottom_blob.max_x, bottom_blob.min_y],
+								[bottom_side_blob.min_x, bottom_side_blob.min_y], [top_side_blob.min_x, top_side_blob.max_y]]
 					else
 						#if we have the top right corner as the top blob
-						return 	[[top_blob.min_x, top_blob.max_y], [bottom_side_blob.max_x, bottom_side_blob.min_y]]
+						return  [[top_side_blob.max_x, top_side_blob.max_y], [bottom_side_blob.max_x, bottom_side_blob.min_y],
+								[bottom_blob.min_x, bottom_blob.min_y], [top_blob.min_x, top_blob.max_y]]
 					end
 
 					#	return [top_blob, bottom_blob, top_side_blob, bottom_side_blob]
@@ -183,77 +185,67 @@ while 1
 =end
 
 #=begin
-i = 3
-while i < 9
+i = 6
+while i <= 81
 	filename = "shot" + (sprintf '%04d',i) + ".png"
 
 #=end
 #	filename = "shot0032.png"
 	puts "loading new file: " + filename
-	img_list = ImageList.new("shots/#{filename}")
-	img_list.rotate!(180)
+	image = ChunkyPNG::Image.from_file("shots/#{filename}")
+	blob_img = image.dup
+	width = image.dimension.width
+
+#	img_list = ImageList.new("shots/#{filename}")
+#	img_list.rotate!(180)
 #	%x[`rm shots/*.png`]
-	blob_view = Image::View.new(img_list, 0, 0, img_list.cur_image.columns, img_list.cur_image.rows)
-	orig_img_view = Image::View.new(img_list, 0, 0, img_list.cur_image.columns, img_list.cur_image.rows)
+#	blob_view = Image::View.new(img_list, 0, 0, img_list.cur_image.columns, img_list.cur_image.rows)
+#	orig_img_view = Image::View.new(img_list, 0, 0, img_list.cur_image.columns, img_list.cur_image.rows)
 
 	blobs = []
-	width = img_list.bounding_box.width-1
-	height = img_list.bounding_box.height-1
+#	width = img_list.bounding_box.width-1
+#	height = img_list.bounding_box.height-1
 
-	blob_view[][].each do |pixel|
-		if (2 * pixel.red) - (pixel.green + pixel.blue) > quantumify(125)
-#			pixel = Pixel.new(quantumify(255), 0, 0)
-			pixel.red  = quantumify(255)
-			pixel.blue = pixel.green = 0
-		else
-			pixel.red = pixel.green = pixel.blue = 0
+	cur_x = 0
+	cur_y = 0
+
+	blob_img.pixels.each do |pixel|
+		redness = calc_redness(ChunkyPNG::Color.r(pixel), ChunkyPNG::Color.g(pixel), ChunkyPNG::Color.b(pixel))
+		if redness >= @min_redness
+			find_blob(blob_img, blobs, cur_x, cur_y)
+		end
+
+		cur_x += 1
+		if cur_x == width
+			cur_y += 1
+			cur_x = 0
 		end
 	end
 
+#	puts "saving blob image"
+#	blob_img.save("output/#{filename}_blob.png")
 
-#	img_list.write("output/#{filename}_orig.png")
-#	blob_view.sync
-#	img_list.write("output/#{filename}_blobs.png")
-
-	for cur_y in (0..height) do
-		for cur_x in (0..width) do
-			if blob_view[cur_y][cur_x].red == quantumify(255)
-				find_blob(blob_view, img_list, blobs, cur_x, cur_y)
-			end
-		end
-	end
-
-
-#blob debug code
+=begin
 	blobs.each do |b|
-		b.points.each do |p|	
-			orig_img_view[p[1]][p[0]] = Pixel.new(quantumify(255), quantumify(255), quantumify(255))
+		b.points.each do |point|
+			image[point[0], point[1]] = ChunkyPNG::Color.rgb(0, 255, 0)
 		end
 	end
+=end
 
-#	orig_img_view.sync
-#	img_list.write("output/#{filename}")
-
-	boundary = find_bounding_rect(orig_img_view, img_list, blobs)
+#	puts "saving image"
+#	image.save("output/#{filename}")
+	
+	boundary = find_bounding_rect(image, blobs)
 		
 	if !boundary.nil?
-#		painter = Magick::Draw.new
-#		painter.stroke('green')
-#		painter.stroke_width(3)
-#		puts boundary.inspect
-#		painter.rectangle(boundary[0][0], boundary[0][1], boundary[1][0], boundary[1][1])
-#		painter.draw(img_list)img_list
-		width = boundary[1][0] - boundary[0][0];
-		height = boundary[1][1] - boundary[0][1];
-#		puts "x : " + boundary[0][0].to_s + ", y : " + boundary[0][1].to_s + ", width: " + width.to_s + " , height: " + height.to_s
-		puts "cropping..."
-		img_list.crop!(boundary[0][0], boundary[0][1], width, height)
-		img_list.write("output/#{filename}")
+		poly_vector = ChunkyPNG::Vector(boundary[0][0], boundary[0][1], boundary[1][0], boundary[1][1],
+										boundary[2][0], boundary[2][1], boundary[3][0], boundary[3][1])
+		image.polygon(poly_vector, ChunkyPNG::Color.rgb(255, 0, 0))
+		image.save("output/#{filename}")
+	elsif(blobs.length > 0)
+		image.save("output/#{filename}")
 	end
-
-#	if(blobs.length > 0)
-#		img_list.write("output/#{filename}")
-#	end
 
 	old_filename = filename
 	i += 1
